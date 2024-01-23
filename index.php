af46c11e5857b48360ddf91eae713f24280defdd
yassinnee <144789414+yassinnee@users.noreply.github.com>
2024-01-23 12:16:31 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Exemple PHP</title>
+</head>
+<body>
+    <h1>Bonjour vous</h1>
+    <p>Nous sommes le <?php echo date("d/m/Y") ?></p>
+</body>
+</html>
